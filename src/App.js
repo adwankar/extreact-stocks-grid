44f55e68edbed527a60df0c658064255ed2432ad
Sandeep Adwankar <sandeepadwankar@sandeep.local>
2017-07-23 11:34:19 -0700

    adding stocks grid component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import { Container } from '@extjs/ext-react';
+import StocksGrid from './StocksGrid';
 
 // Enable responsiveConfig app-wide. You can remove this if you don't plan to build a responsive UI.
 Ext.require('Ext.plugin.Responsive');
 
-export default class App extends Component {
 
-    render() {
-        return (
-            <Container fullscreen>
-                StocksGrid
-            </Container>
-        )
-    }
-
+export default function App() {
+    return (
+        <Container fullscreen layout="fit">
+            <StocksGrid/>
+        </Container>
+    )
 }
